dbda996ec04ba8fb6c6a709a74318cee27990197
Andrew Crotty <andrew.crotty@northwestern.edu>
2024-10-31 23:11:04 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# deepsqueeze
+# DeepSqueeze
+
+This repository contains a basic implementation of DeepSqueeze from our [SIGMOD 2020 paper](https://dl.acm.org/doi/10.1145/3318464.3389734). DeepSqueeze is a semantic compression framework that uses autoencoders to capture complex relationships in real-world tabular datasets, in particular focusing on error-bounded lossy compression of numerical data.
+
+This implementation does not include some advanced techniques described in the paper, such as parameter sharing for categorical attributes, gated mixture of experts, and Bayesian optimization for hyperparameter tuning. If you are interested in these aspects of the work, please see this (unaffiliated) implementation: https://github.com/MikeXydas/DeepSqueeze
+
+You can use the following commands to reproduce the results from the paper:
+```
+# Corel
+python3 deepsqueeze.py data/corel.csv -c brotli -l 11 -e 0.005 -E 1500
+python3 deepsqueeze.py corel.csv.tar.gz -d -C data/corel.csv
+
+# Forest
+python3 deepsqueeze.py data/forest.csv -c brotli -l 11 -e 0.005 -E 300
+python3 deepsqueeze.py forest.csv.tar.gz -d -C data/forest.csv
+```
+
+If you use this code, please cite:
+```
+@inproceedings{DBLP:conf/sigmod/IlkhechiCGMFSC20,
+  author       = {Amir Ilkhechi and
+                  Andrew Crotty and
+                  Alex Galakatos and
+                  Yicong Mao and
+                  Grace Fan and
+                  Xiran Shi and
+                  Ugur {\c{C}}etintemel},
+  editor       = {David Maier and
+                  Rachel Pottinger and
+                  AnHai Doan and
+                  Wang{-}Chiew Tan and
+                  Abdussalam Alawini and
+                  Hung Q. Ngo},
+  title        = {DeepSqueeze: Deep Semantic Compression for Tabular Data},
+  booktitle    = {Proceedings of the 2020 International Conference on Management of
+                  Data, {SIGMOD} Conference 2020, online conference [Portland, OR, USA],
+                  June 14-19, 2020},
+  pages        = {1733--1746},
+  publisher    = {{ACM}},
+  year         = {2020},
+  url          = {https://doi.org/10.1145/3318464.3389734},
+  doi          = {10.1145/3318464.3389734},
+  timestamp    = {Wed, 04 May 2022 13:02:28 +0200},
+  biburl       = {https://dblp.org/rec/conf/sigmod/IlkhechiCGMFSC20.bib},
+  bibsource    = {dblp computer science bibliography, https://dblp.org}
+}
+```
---
+++ deepsqueeze.py
@@ -0,0 +1,117 @@
+import argparse
+import keras
+import os
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import sys
+
+EPS = sys.float_info.epsilon
+
+def scale(df):
+    range_ = df.agg(['min', 'max'])
+    df = (df - range_.iloc[0]) / (range_.iloc[1] - range_.iloc[0] + EPS)
+    return df, range_
+
+def to_parquet(df, file, compression, level):
+    pq.write_table(pa.Table.from_pandas(df, preserve_index=False), file,
+                   use_dictionary=True, compression=compression,
+                   write_statistics=False, compression_level=level,
+                   data_page_version='2.0', store_schema=False)
+    return os.path.getsize(file)
+
+def compress(file, batch, compression, delimiter, error, epochs, level, loss,
+             output, **kwargs):
+    out = f'.{file.split("/")[-1] if output is None else output}'
+    os.system(f'rm -rf {out} && mkdir {out}')
+    pd.DataFrame([error]).to_csv(f'{out}/error.csv', index=False)
+
+    X, range_ = scale(pd.read_csv(file, sep=delimiter))
+    range_.to_csv(f'{out}/range.csv', index=False)
+
+    encoder = keras.Sequential([
+                  keras.Input((X.shape[1],)),
+                  keras.layers.Dense(X.shape[1] * 2, activation='leaky_relu'),
+                  keras.layers.Dense(X.shape[1] * 2, activation='leaky_relu'),
+                  keras.layers.Dense(1, activation='sigmoid')
+              ], name='encoder')
+    decoder = keras.Sequential([
+                  keras.Input((1,)),
+                  keras.layers.Dense(X.shape[1] * 2, activation='leaky_relu'),
+                  keras.layers.Dense(X.shape[1] * 2, activation='leaky_relu'),
+                  keras.layers.Dense(X.shape[1], activation='sigmoid')
+              ], name='decoder')
+    autoencoder = keras.Sequential([encoder, decoder])
+    autoencoder.compile('adam', loss)
+    autoencoder.fit(X, X, batch_size=batch, epochs=epochs)
+    decoder.save(f'{out}/decoder.keras')
+
+    prev = sys.maxsize
+    for i in range(10):
+        Z = encoder.predict(X).round(i)
+        codes = pd.DataFrame(Z * 10.0 ** i).astype(int)
+        size = to_parquet(codes, f'{out}/codes-{i}.pq', compression, level)
+
+        X_ = pd.DataFrame(decoder.predict(Z), columns=X.columns)
+        deltas = ((X - X_) / error).astype(int)
+        size += to_parquet(deltas, f'{out}/deltas-{i}.pq', compression, level)
+
+        if size > prev:
+            pd.DataFrame([i - 1]).to_csv(f'{out}/quantize.csv', index=False)
+            os.system(f'rm {out}/*-{i}.pq')
+            break
+
+        prev = size
+        if i != 0:
+            os.system(f'rm {out}/*-{i - 1}.pq')
+
+    os.system(f'tar czf {out[1:]}.tar.gz {out} && rm -rf {out}')
+
+def decompress(file, check, delimiter, output, **kwargs):
+    out = f'.{file.split("/")[-1].split(".tar.gz")[0]}'
+    os.system(f'tar xzf {file}')
+
+    quantize = pd.read_csv(f'{out}/quantize.csv').iloc[0, 0]
+    codes = pd.read_parquet(f'{out}/codes-{quantize}.pq')
+    Z = codes / 10.0 ** quantize
+
+    decoder = keras.saving.load_model(f'{out}/decoder.keras')
+    X_ = decoder.predict(Z)
+
+    error = pd.read_csv(f'{out}/error.csv').iloc[0, 0]
+    deltas = pd.read_parquet(f'{out}/deltas-{quantize}.pq')
+    X_ = X_ + deltas * error
+
+    if check is not None:
+        X, _ = scale(pd.read_csv(check, sep=delimiter))
+        diffs = (X - X_).abs() >= error
+        if diffs.any().any():
+            print(f'Columns exceeding {error} error:\n{diffs.any()}')
+
+    range_ = pd.read_csv(f'{out}/range.csv')
+    X_ = (X_ + range_.iloc[0]) * (range_.iloc[1] - range_.iloc[0] + EPS)
+    X_.to_csv(out[1:] if output is None else output, index=False)
+    os.system(f'rm -rf {out}')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file')
+    parser.add_argument('-b', '--batch', default=64, type=int)
+    parser.add_argument('-c', '--compression', default='gzip')
+    parser.add_argument('-C', '--check')
+    parser.add_argument('-d', '--decompress', action='store_true')
+    parser.add_argument('-D', '--delimiter', default=',')
+    parser.add_argument('-e', '--error', default=1e-3, type=float)
+    parser.add_argument('-E', '--epochs', default=10, type=int)
+    parser.add_argument('-l', '--level', default=6, type=int)
+    parser.add_argument('-L', '--loss', default='mae')
+    parser.add_argument('-o', '--output')
+    args = parser.parse_args()
+
+    if args.decompress:
+        decompress(**vars(args))
+    else:
+        compress(**vars(args))
+
+if __name__ == '__main__':
+    main()
